d943bb5b7004f834119715e13f5819bfe8b9379f
LaChimere <chingkun_lin@126.com>
2021-02-08 23:17:56 +0800

    fix: elapsed spelling

--- 01-Tutorial/time_cmp.go
+++ 01-Tutorial/time_cmp.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    // "strings"
-    "time"
+	"fmt"
+	"os"
+	// "strings"
+	"time"
 )
 
 func main() {
-    start := time.Now()
+	start := time.Now()
 
-    // fmt.Println(strings.Join(os.Args[1:], " "))
+	// fmt.Println(strings.Join(os.Args[1:], " "))
 
-    var s, sep string
-    for _, arg := range os.Args[1:] {
-        s += sep + arg
-        sep = " "
-    }
-    fmt.Println(s)
+	var s, sep string
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
 
-    t := time.Now()
-    elasped := t.Sub(start)
+	t := time.Now()
+	elapsed := t.Sub(start)
 
-    fmt.Println(elasped)
+	fmt.Println(elapsed)
 }
-
