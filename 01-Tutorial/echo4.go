22fdaf43b8036c14173cb257e10e0348f288c549
LaChimere <chingkun_lin@126.com>
2021-02-08 13:42:21 +0800

    add: echoes

---
+++ 01-Tutorial/echo4.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    for idx, arg := range os.Args[:] {
+        // fmt.Printf("%d: %s\n", idx, arg)
+        fmt.Println(idx, arg)
+    }
+}
+
