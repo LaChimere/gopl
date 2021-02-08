7f783a0f38a3e91105ccb7d97b7746b6b6958aad
LaChimere <chingkun_lin@126.com>
2021-02-08 13:42:55 +0800

    add: running time compare

---
+++ 01-Tutorial/time_cmp.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    // "strings"
+    "time"
+)
+
+func main() {
+    start := time.Now()
+
+    // fmt.Println(strings.Join(os.Args[1:], " "))
+
+    var s, sep string
+    for _, arg := range os.Args[1:] {
+        s += sep + arg
+        sep = " "
+    }
+    fmt.Println(s)
+
+    t := time.Now()
+    elasped := t.Sub(start)
+
+    fmt.Println(elasped)
+}
+
