5951828de67e8eb6eeac9a72a406061865387fb0
LaChimere <chingkun_lin@126.com>
2021-02-10 16:20:04 +0800

    add: pc.go

---
+++ 02-ProgramStructure/pc.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"golang-tutorial/02-ProgramStructure/popcount"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	x, _ := strconv.Atoi(input.Text())
+	fmt.Println(popcount.PopCount(uint64(x)))
+}
