9ecd948190a252399b43057fc59164f28e58d12d
LaChimere <chingkun_lin@126.com>
2021-02-12 22:08:01 +0800

    modify: cf.go

--- 02-ProgramStructure/cf.go
+++ 02-ProgramStructure/cf.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 
-    "golang-tutorial/02-ProgramStructure/tempconv"
+	"gopl/02-ProgramStructure/tempconv"
 )
 
 func main() {
-    for _, arg := range os.Args[1:] {
-        t, err := strconv.ParseFloat(arg, 64)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "cf: %v\n", err)
-            os.Exit(1)
-        }
-        f := tempconv.Fahrenheit(t)
-        c := tempconv.Celsius(t)
-        fmt.Printf("%s = %s, %s = %s\n", f, tempconv.F2C(f), c, tempconv.C2F(c))
-    }
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.F2C(f), c, tempconv.C2F(c))
+	}
 }
-
