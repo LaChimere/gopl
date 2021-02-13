efe15fa73bb3769aa37ce5f72b3eb4a6a1754935
LaChimere <chingkun_lin@126.com>
2021-02-13 22:27:09 +0800

    add: append.go

---
+++ 04-CompositeTypes/append.go
@@ -0,0 +1,21 @@
+package main
+
+func appendInt(x []int, y ...int) []int {
+	var z []int
+	zlen := len(x) + len(y)
+	if zlen <= cap(x) {
+		// There's room to grow. Extend the slice.
+		z = x[:zlen]
+	} else {
+		// There's insufficient space. Allocate a new array.
+		// Grow by doubling, for amortized linear complexity.
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	copy(z[len(x):], y)
+	return z
+}
