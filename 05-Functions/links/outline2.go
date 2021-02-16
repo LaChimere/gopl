cf3b287c04ff372d8012c15377c31977a3898b37
LaChimere <chingkun_lin@126.com>
2021-02-16 18:02:49 +0800

    mv: outline2.go moved to pkg links

---
+++ 05-Functions/links/outline2.go
@@ -0,0 +1,23 @@
+// forEachNode calls the functions pre(x) and post(x) for each node
+// x in the tree rooted at n. Both functions are optional.
+// pre is called before the children are visited (preorder) and
+// post is called after (postorder).
+package links
+
+import (
+	"golang.org/x/net/html"
+)
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
